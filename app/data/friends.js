67bd1dbb57a0850a176215995d20af0efe3c8838
Sergey60327 <sergeyshcherbakov@Employees-MacBook-Air-109.local>
2017-04-28 16:02:14 -0700

    Update2

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friendArray=[
+  {
+        name: "Mickey",
+        photo: 'http://www.disney.co.uk/sites/default/files/Mickey/GENERIC/Synopsis%20Image/MM_cutout.jpg',
+        question: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      },{
+        name: "Minnie",
+        photo: 'http://www.hdwallpaper.nu/wp-content/uploads/2015/02/Disney-Sweet-Cartoon-Minnie-Mouse-Wallpaper.jpg',
+        question: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+      },{
+        name: "Goofy",
+        photo: 'http://imgc.allpostersimages.com/images/P-488-488-90/77/7712/3RO1300Z/posters/goofy-face-mask.jpg',
+        question: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      }];
+
+module.exports = friendArray;

f77fef783435db78b9987717699ae020634d1e45
dblanchard13 <davidb.prof@gmail.com>
2017-05-19 21:57:03 -0700

    Homework Comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 var friendsData  = require('../data/friends.js');
 
-var path = require('path');
+// you aren't actually using the path module in this file so there's no need to require it
+// var path = require('path');
 
 module.exports = function(app){
 
@@ -13,10 +14,13 @@ app.post('/api/friends', function(req, res){
 var closestMatch = 0;
 var maxDiff = 1000;
 
+// since friendsData is an array you could just use the native `.forEach` method here
+// this creates a functional closure/scope that won't leak variables.
 for (var i = friendsData.length - 1; i >= 0; i--) {
 
 var totalDifference = 0;
 
+// you could also use forEach here
 for (var j = 0; j < 2; j++ ){
 
   totalDifference = totalDifference + Math.abs(friendsData[i].scores[j] - req.body.scores[j]);
@@ -24,6 +28,7 @@ for (var j = 0; j < 2; j++ ){
         }
 
         if (totalDifference < maxDiff){
+          // maxDiff is a pretty confising variable name since it's really the minimum difference that you're keeping track of
           maxDiff = totalDifference;
           closestMatch = i;
         }
